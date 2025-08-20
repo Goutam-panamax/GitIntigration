73e63f22d6993ae8e7875ea119890336a7e3c342
Goutam-panamax <111481793+Goutam-panamax@users.noreply.github.com>
2025-08-20 18:53:56 +0530

    cleared

--- Files/Sprint24_Queries.sql
+++
@@ -1,9 +0,0 @@
-SET @staticValueSource = (  SELECT `Value` FROM `TBLMStandardMasterDetail` WHERE `UID` = 'SUGGESTION_BASED_TEXTBOX');Add commentMore actions
-
-SET @NotificationBlockId = (SELECT `BlockId` FROM `TBLMBuildingBlocks`  WHERE `UID` = 'PREPARENOTIFICATION');
-
-UPDATE `TBLMBuildingBlocksProperties`SET `ComponentType` = @staticValueSource WHERE `BlockId` = @NotificationBlockId AND UID="BODY_PREPARENOTIFICATION";
-
-UPDATE TBLMBuildingBlocksProperties SET RegularExpression = '^[A-Za-z0-9]+$', ValidationMessage = 'Only letters and numbers allowed' WHERE Name = 'OutputPersistKey';Add commentMore actions
-
-ALTER TABLE TBLMCounter MODIFY COLUMN ResetType TINYINT(1) NOT NULL;
